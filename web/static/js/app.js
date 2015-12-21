4e165a8536ec9de7c64cb8dfec72448f124376d2
terry-dellino <terry.dellino@polarmobile.com>
2015-12-21 12:12:16 -0500

    Support less and bootstrap

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,2 +1,1 @@
-import "phoenix_html"
 console.log('webpack compiled me at: ' + Date.now());
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+"use strict";
+
+var path = require('path');
+var webpack = require('webpack');
+var node_modules_dir = path.resolve(__dirname, 'node_modules');
+var root = path.resolve(__dirname, 'web/static/');
+var js_dir = path.resolve(root, 'js');
+var css_dir = path.resolve(root, 'css');
+
+
 var CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: ["./web/static/js/app.js", "./web/static/css/app.css"],
+  devtool: "source-map",
+  entry: {
+    "app": [path.resolve(css_dir, "app.less"), path.resolve(js_dir, "app.js")],
+  },
   output: {
     path: "./priv/static/js",
     filename: "app.js"
   },
 
   resolve: {
-    modulesDirectories: [__dirname + "/web/static/js"],
+    modulesDirectories: [js_dir],
     alias: {
-      phoenix_html: __dirname + "/deps/phoenix_html/web/static/js/phoenix_html.js",
       phoenix: __dirname + "/deps/phoenix/web/static/js/phoenix.js"
     }
   },
@@ -20,27 +31,45 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: [node_modules_dir],
         loader: "babel",
-        include: __dirname
+        include: __dirname,
         query: {
           presets: ["es2015"]
         }
       },
       {
         test: /\.jsx$/,
-        exclude: /node_modules/,
+        exclude: [node_modules_dir],
         loaders: ["react-hot", "babel-loader"],
       },
       {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract("css")
+        test: /\.less$/,
+        exclude: [node_modules_dir],
+        loader: 'style!css!less'
       },
+
+      { test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
     ],
   },
 
   plugins: [
-    new ExtractTextPlugin("css/app.css")
-    new CopyWebpackPlugin([{ from: "./web/static/assets" }])
+    new CopyWebpackPlugin([
+      { from: "./web/static/assets" },
+      { from: "./deps/phoenix_html/web/static/js/phoenix_html.js",
+        to: "js/phoenix_html.js" }
+    ])
   ]
 };
+
+// if running webpack in production mode, minify files with uglifyjs
+if (process.env.NODE_ENV === "production") {
+  config.plugins.push(
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({ minimize: true })
+  );
+}
